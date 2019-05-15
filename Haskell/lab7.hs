2effce9f8cf1f63bcdfed2da52d71c39eff1f7a8
VladislavDuma <doomvlad@gmail.com>
2019-05-15 23:22:12 +0300

    Update lab7.hs

--- Haskell/lab7.hs
+++ Haskell/lab7.hs
@@ -1,4 +1,12 @@
+--Задача 7
+--Реализовать на языке Haskell функцию, заменяющую в исходном списке все вхождения заданного значения другим
+
 myreplace _ _ [] = []
 myreplace cur new (h:t)
     | h == cur  = new : myreplace cur new t
     | otherwise = h : myreplace cur new t
+    
+--myreplace 1 2 [1, 2, 3]
+--[2,2,3]
+--myreplace "a" "c" ["b", "a", "d"]
+--["b","c","d"]
