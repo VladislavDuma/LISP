e21fd4f9b0418a7115d9de5c5e240a287a0ffbe4
VladislavDuma <doomvlad@gmail.com>
2019-05-15 21:51:26 +0300

    Update lab5.hs

--- Haskell/lab5.hs
+++ Haskell/lab5.hs
@@ -1,5 +1,13 @@
+--Задача 5
+--Реализовать на языке Haskell функцию, заменяющую в исходном списке два подряд идущих одинаковых элемента одним
+
 myreduce [] = []
 myreduce [el] = [el]
 myreduce (h:t) 
     | h == head t = myreduce (h:(tail t))
     | otherwise   = h:myreduce t
+
+--myreduce [1, 1, 1, 2, 2]
+--[1,2]
+--myreduce [1, 1, 1, 2, 2, 3, 4, 2]
+--[1,2,3,4,2]
